37ee668b85350454ade8272bd1fb9bb0221b5e7f
aralinilya <aralinilya@aralinilya.local>
2016-10-06 00:11:15 +0300

    add

--- YA.js
+++ YA.js
@@ -15,74 +15,95 @@ YA.__elems = [];
  */
 YA.Element = function (options) {
   var _this = this;
-  this.options = {
-    namespace: options.namespace || 'http://www.w3.org/1999/xhtml',
-    parent: options.parent || null,
-    tag: options.tag || 'div',
-    content: options.content || '',
-    attrs: options.attrs || {},
-    events: options.events || {}
-  };
 
-  function create() {
-    var options = _this.options, elem;
+  this.namespace = options.namespace || 'http://www.w3.org/1999/xhtml';
+  this.elem = null;
+  this.parent = options.parent || null;
+  this.tag = options.tag || 'div';
+  this.content = options.content || '';
+  this.attrs = options.attrs || {};
+  this.events = options.events || {};
 
-    if (YA.f.ifMatch(options.tag, 'text')) {
-      elem = document.createTextNode(options.content);
-    } else {
-      elem = document.createElementNS ? document.createElementNS(options.namespace, options.tag) : document.createElement(options.tag);
-      elem.innerHTML = options.content;
+  this.setAttr = function (key, val) {
 
-      for (var _attr in options.attrs) {
+    switch (key) {
 
-        switch (_attr) {
+    case 'class':
 
-        case 'class':
-          if (YA.f.ifExist(options.attrs[_attr], 'object')) {
-            for (var _class = 0; _class < options.attrs[_attr].length; _class++) {
-              elem.classList.add(options.attrs[_attr][_class]);
-            }
-          } else {
-            elem.classList.add(options.attrs[_attr]);
-          }
-          break;
-
-        case 'style':
-          if (YA.f.ifExist(options.attrs[_attr], 'object')) {
-            for (var _style in options.attrs[_attr]) {
-              if (YA.f.ifExist(options.attrs[_attr][_style])) elem.style[_style] = options.attrs[_attr][_style];
-            }
-          } else {
-            elem.setAttribute('style', options.attrs[_attr]);
-          }
-          break;
-
-        default:
-          if (YA.f.ifExist(options.attrs[_attr])) elem.setAttribute(_attr, options.attrs[_attr]);
-          break;
+      if (YA.f.ifExist(val, 'object')) {
+        for (var _class = 0; _class < val.length; _class++) {
+          _this.elem.classList.add(val[_class]);
+          _this.attrs[key] = val[_class];
         }
+      } else {
+        _this.elem.classList.add(val);
+        _this.attrs[key] = val;
+      }
+      break;
+
+    case 'style':
+      if (YA.f.ifExist(val, 'object')) {
+        for (var _style in val) {
+          if (YA.f.ifExist(val[_style])) {
+            _this.elem.style[_style] = val[_style];
+            _this.attrs[key][_style] = val[_style];
+          }
+        }
+      } else {
+        _this.elem.setAttribute('style', val);
+        _this.attrs[key] = val;
+      }
+      break;
+
+    default:
+      if (YA.f.ifExist(val)) {
+        _this.elem.setAttribute(key, val);
+        _this.attrs[key] = val;
+
+      }
+      break;
+    }
+  }
+
+  this.getAttr = function (attr) {
+    return _this.elem.getAttribute(attr);
+  }
+  
+  this.removeAttr = function (attr) {
+    _this.elem.removeAttribute(attr);
+  }
+  
+  function create() {
+
+    if (YA.f.ifMatch(_this.tag, 'text')) {
+      _this.elem = document.createTextNode(_this.content);
+    } else {
+      _this.elem = document.createElementNS ? document.createElementNS(_this.namespace, _this.tag) : document.createElement(_this.tag);
+      _this.elem.innerHTML = _this.content;
+
+      for (var _attr in _this.attrs) {
+        _this.setAttribute(_attr, _this.attrs[_attr]);
+
       }
 
-      for (var _event in options.events) {
-        
-        elem.addEventListener(_event, options.events[_event]);
+      for (var _event in _this.events) {
+        _this.elem.addEventListener(_event, _this.events[_event]);
       }
     }
 
-    _this.elem = elem;
 
     if (YA.f.ifHtml(options.parent)) {
       options.parent = (YA.f.ifMatch(options.parent.nodeName, '#text')) ? options.parent.parentNode : options.parent;
 
-      options.parent.appendChild(elem);
+      options.parent.appendChild(_this.elem);
     }
   }
 
   this.remove = function () {
-    for(var i =0; i < YA.__elems.length; i++){
+    for (var i = 0; i < YA.__elems.length; i++) {
       if (YA.__elems[i].elem !== _this.elem) continue;
       YA.__elems[i].elem.parentNode.removeChild(YA.__elems[i].elem);
-      YA.__elems.slice(i,1);
+      YA.__elems.slice(i, 1);
     }
   }
 
@@ -92,14 +113,15 @@ YA.Element = function (options) {
 }
 
 YA.Block = function (obj) {
-  var _this = this, _hidden = {};
+  var _this = this,
+    _hidden = {};
   this.tree = obj;
   this.elem = null;
 
-  _hidden.create = function(parent, obj) {
-    
+  _hidden.create = function (parent, obj) {
+
     for (var _elem = 0; _elem < obj.length; _elem++) {
-      
+
       var _ifNode = YA.f.ifExist(obj[_elem].content, 'object');
 
       var element = new YA.Element({
@@ -112,14 +134,14 @@ YA.Block = function (obj) {
       });
 
       if (!_this.elem) _this.elem = element.elem;
-      
+
       if (_ifNode) _hidden.create(element.elem, obj[_elem].content);
     }
 
   }
   _hidden.create(obj.parent, obj.content);
   return this;
-  
+
 }
 
 YA.f = {};
@@ -160,4 +182,3 @@ YA.f.ifMatch = function (a, b) {
 YA.f.ifExist = function (str, type) {
   return (str === undefined || str === null) ? false : ((type !== undefined) ? (typeof str === type) : true);
 };
-
